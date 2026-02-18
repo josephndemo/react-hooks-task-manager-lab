103ff7f2933dc5733fe4b0ea2a52da189cdda732
Joseph Ndemo <joseph.ndemo@student.moringaschool.com>
2026-02-17 23:24:55 -0500

    updated styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { TaskProvider } from "./context/TaskContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <TaskProvider>
